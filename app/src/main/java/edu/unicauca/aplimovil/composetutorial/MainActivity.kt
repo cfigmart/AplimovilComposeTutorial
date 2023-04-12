f8e5b9ab33055c64d1f34f5c15af7b33ea2568a3
Cristhian Figueroa <cfigmart@users.noreply.github.com>
2023-04-12 16:14:28 -0500

    Completa la aplicación con LazyColum pero aún falta modificar nombres y fotos de los colegas

--- app/src/main/java/edu/unicauca/aplimovil/composetutorial/MainActivity.kt
+++ app/src/main/java/edu/unicauca/aplimovil/composetutorial/MainActivity.kt
@@ -1,16 +1,22 @@
 package edu.unicauca.aplimovil.composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 
@@ -26,9 +32,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeTutorialTheme() {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(
-                        msg = Message("Maria", "Hola this is the body")
-                    )
+                    Conversation(messages = SampleData.conversationSample)
                 }
 
             }
@@ -52,27 +56,65 @@ fun MessageCard(msg: Message){
                 .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
+
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
+
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary
+            else MaterialTheme.colors.surface
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
 
             )
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    style = MaterialTheme.typography.body2,
+                    maxLines = if(isExpanded) 100 else 1,
+                    modifier = Modifier.padding(all= 4.dp)
+                )
+            }
+
         }
     }
 
 
 }
 
-@Preview
+@Composable
+fun Conversation(messages: List<Message>){
+    LazyColumn{
+        items(messages){message ->
+            MessageCard(msg = message)
+        }
+    }
+
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name= "Dark mode"
+)
 @Composable
 fun PreviewMessageCard(){
     ComposeTutorialTheme() {
         Surface(modifier = Modifier.fillMaxSize()) {
-            MessageCard(
-                msg = Message("Maria", "Hello this is the body")
-            )
+            Conversation(messages = SampleData.conversationSample)
         }
 
     }
